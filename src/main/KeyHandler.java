5bb048cd7ad8d0c97e2aaa9566272e6b4c503ad0
Cleberpsanches <jscleberpsanches@gmail.com>
2025-06-10 01:56:01 -0300

    Sound fixed

--- src/main/KeyHandler.java
+++ src/main/KeyHandler.java
@@ -87,7 +87,6 @@ public class KeyHandler implements KeyListener {
         if (code == KeyEvent.VK_ENTER){
             if(gp.ui.commandNum == 0){
                 gp.gameState = gp.playState;
-                gp.playMusic(0);
             }
             if (gp.ui.commandNum == 1){
                 //permanência de dados
